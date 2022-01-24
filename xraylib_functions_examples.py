59e59c07bdbc79d3317d0d036f8f2ebda74ed45d
HAFFAR Iheb <haffari@cenmanto4.cen.meteo.fr>
2022-01-24 15:26:19 +0100

    last modif mu

--- xraylib_functions_examples.py
+++ xraylib_functions_examples.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 21 12:58:38 2022
-
-@author: haffari
-"""
 
 #Copyright (c) 2009, 2010, 2011 Tom Schoonjans
 #All rights reserved.
@@ -16,98 +11,60 @@ Created on Fri Jan 21 12:58:38 2022
 #    * The names of the contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
 
 #THIS SOFTWARE IS PROVIDED BY Tom Schoonjans ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Tom Schoonjans BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+Created on Fri Jan 21 12:58:38 2022
 
+@author: haffari
+"""
 """Example of using various xraylib functionality in python."""
 
 import xraylib
 import math
 import numpy as np
 
+## converting atomic numbr to symbol
+symbol = xraylib.AtomicNumberToSymbol(13)
+print(symbol)
 
-test = xraylib.AtomicNumberToSymbol(13)
-print(test)
+## converting symbol of an element to its atmic number
+atomic_number = xraylib.SymbolToAtomicNumber('Al')
+print(atomic_number)
 
-test_x = xraylib.ElementDensity(13)
-print(test_x)
+## calculating the density of an element: requires atomic number
+density_element = xraylib.ElementDensity(13)
+print(density_element)
 
-test4 = xraylib.SymbolToAtomicNumber('Al')
-print(test4)
+## compound parser: (1) calculating characteristic of an element 
+compound_element = xraylib.CompoundParser('Al')
+print(compound_element)
+## compound parser: (2) calculating characteristic of a chemical formula
+compound_formula = xraylib.CompoundParser('C10H7Cl')
+print(compound_formula)
 
-test2 = xraylib.CompoundParser("Al")['Elements'][0]
-print(test2)
+## example of the properties of a chemical product
+compound = "H2O"
+compound_properties = xraylib.CompoundParser("H2O")
+print("{} contains {} atoms, {} elements and has a molar mass of {} g/mol".format(compound, compound_properties['nAtomsAll'], compound_properties['nElements'], compound_properties['molarMass']))
+for i in range(compound_properties['nElements']):
+    print("Element {} ({}): {} % and {} atoms".format(compound_properties['Elements'][i],xraylib.AtomicNumberToSymbol(compound_properties['Elements'][i]), 'mass Fraction = ' + str(round(compound_properties['massFractions'][i]*100.0,2)), compound_properties['nAtoms'][i]))
 
-test3 = xraylib.ElementDensity(xraylib.CompoundParser("Al")['Elements'][0])
-print(test3)
-
-list_symbols = []
-for i in range(1,99):
-    symbol = xraylib.AtomicNumberToSymbol(i)
-    density = xraylib.ElementDensity(int(i))
-    list_symbols.append((symbol,density))
-print(list_symbols)
-
-
-xraylib.XRayInit()
-
-print("Example of python program using xraylib")
-print("xraylib version: {}".format(xraylib.__version__))
-print("Density of pure Al : {} g/cm3".format(xraylib.ElementDensity(13)))
-
-
-
-
-cdtest = xraylib.CompoundParser("Ca(HCO3)2")
-print("Ca(HCO3)2 contains {} atoms, {} elements and has a molar mass of {} g/mol".format(cdtest['nAtomsAll'], cdtest['nElements'], cdtest['molarMass']))
-
-
-for i in range(cdtest['nElements']):
-    print("Element {}: {} % and {} atoms".format(cdtest['Elements'][i], cdtest['massFractions'][i]*100.0, cdtest['nAtoms'][i]))
-
-cdtest = xraylib.CompoundParser("SiO2")
-print("SiO2 contains {} atoms, {} elements and has a molar mass of {} g/mol".format(cdtest['nAtomsAll'], cdtest['nElements'], cdtest['molarMass']))
-for i in range(cdtest['nElements']):
-    print("Element {}: {} % and {} atoms".format(cdtest['Elements'][i], 'mass Fraction = ' + str(cdtest['massFractions'][i]*100.0), cdtest['nAtoms'][i]))
-
-
-
-symbol = xraylib.AtomicNumberToSymbol(26)
-print("Symbol of element 26 is: {}".format(symbol))
-print("Number of element Fe is: {}".format(xraylib.SymbolToAtomicNumber("Fe")))
-
-
-cdn = xraylib.GetCompoundDataNISTByName("Uranium Monocarbide")
-print("Uranium Monocarbide")
-print("  Name: {}".format(cdn['name']))
-print("  Density: {}".format(cdn['density']))
-for i in range(cdn['nElements']):
-    print("  Element {}: {} %%".format(cdn['Elements'][i], cdn['massFractions'][i]*100.0))
-
-cdn = xraylib.GetCompoundDataNISTByIndex(xraylib.NIST_COMPOUND_BRAIN_ICRP)
-print("NIST_COMPOUND_BRAIN_ICRP")
-print("  Name: {}".format(cdn['name']))
-print("  Density: {}".format(cdn['density']))
-for i in range(cdn['nElements']):
-    print("  Element {}: {} %%".format(cdn['Elements'][i], cdn['massFractions'][i]*100.0))
-
-nistCompounds = xraylib.GetCompoundDataNISTList()
+## example of all products available in the NIST list
+nist_compounds = xraylib.GetCompoundDataNISTList()
 print("List of available NIST compounds:")
-for i in range(len(nistCompounds)):
-    print("  Compound {}: {}".format(i,nistCompounds[i]))
+for i in range(len(nist_compounds)):
+    print("  Compound {}: {}".format(i,nist_compounds[i]))
 
-print("")
 
-# radioNuclideData tests
-rnd = xraylib.GetRadioNuclideDataByName("109Cd")
-print("109Cd")
-print("  Name: {}".format(rnd['name']))
-print("  Z: {}".format(rnd['Z']))
-print("  A: {}".format(rnd['A']))
-print("  N: {}".format(rnd['N']))
-print("  Z_xray: {}".format(rnd['Z_xray']))
-print("  X-rays:")
+## calling a product of the NIST list
+compound = xraylib.GetCompoundDataNISTByName("Polypropylene")
+print("  Name: {}".format(compound['name']))
+print("  Density: {}".format(compound['density']))
+for i in range(compound['nElements']):
+    print("  Element {} ({}): {} %".format(compound['Elements'][i],xraylib.AtomicNumberToSymbol(compound['Elements'][i]) , round(compound['massFractions'][i]*100.0),2))
 
-------------------- END OF XRLEXAMPLE5 -------------------------")
-# print("")
+
+##------------------ END OF XRLEXAMPLE5 -------------------------")
+
 
 
 
